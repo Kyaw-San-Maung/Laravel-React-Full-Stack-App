a6068b87be8a43365512574f278a17e5f878eb94
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-05-12 14:41:17 +0630

    feat: add api in backend for singup.

--- react/src/components/Signup.jsx
+++ react/src/components/Signup.jsx
@@ -1,8 +1,38 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "./axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Signup() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordconfirmationRef = useRef();
+
+    const { setUser, setToken } = useStateContext();
+
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordconfirmationRef.current.value,
+        };
+
+        axiosClient
+            .post("/signup", payload)
+            .then(({ data }) => {
+                setToken(data.token), setUser(data.name);
+            })
+            .catch((err) => {
+                const response = err.response;
+
+                if (response && response.status == 422) {
+                    console.log(response.data.errors);
+                }
+            });
     };
 
     return (
@@ -10,10 +40,19 @@ export default function Signup() {
             <div className="form">
                 <form action="" onSubmit={onSubmit}>
                     <h1 className="title">Signup For Free</h1>
-                    <input type="email" placeholder="Full Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
+                    <input ref={nameRef} type="text" placeholder="Full Name" />
                     <input
+                        ref={emailRef}
+                        type="email"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordconfirmationRef}
                         type="password"
                         placeholder="Password Confirmation"
                     />
